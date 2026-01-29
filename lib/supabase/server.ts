f32985ea1657f98fe6c17c0fa0cd633b44fffeb8
unknown <aleef2540ss@gmail.com>
2026-01-29 11:49:05 +0700

    fix server bug

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -11,13 +11,7 @@ export async function createClient() {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options: any) {
-          cookieStore.set({ name, value: "", ...options });
-        },
+        }
       },
     }
   );
