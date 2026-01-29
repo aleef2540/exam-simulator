0560f04144426f4e19d7559a3e5c97a3c5f17a6c
unknown <aleef2540ss@gmail.com>
2026-01-29 11:41:44 +0700

    fix

--- components/ui/LogoutButton.tsx
+++ components/ui/LogoutButton.tsx
@@ -8,7 +8,7 @@ export default function LogoutButton() {
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
-    router.replace("/login");
+    router.replace("/");
   };
 
   return (
